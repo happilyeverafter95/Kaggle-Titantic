3c1042ebe96ce7e8f9f8a78b82e7f7f15b692478
Mandy <yymgu@edu.uwaterloo.ca>
2017-09-18 21:55:49 -0400

    Updated comments in Data Treatment file

--- Data Treatment.py
+++ Data Treatment.py
@@ -1,22 +1,25 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
+import seaborn as sns
 import re
 
-# Step 0 part 1: Accessing the training and testing sets 
+# Step 0: Accessing the training and testing sets 
 
 train = pd.read_csv('C:\\Users\\mandy\\Desktop\\Kaggle competitions\\Titanic\\train.csv')
 test = pd.read_csv('C:\\Users\\mandy\\Desktop\\Kaggle competitions\\Titanic\\test.csv')
 
-#Combining training and testing set to obtain an overall look at the data set 
+# Combining training and testing set to obtain an overall look at the data set 
 
 result = [train, test] 
 together = pd.concat(result, join_axes=None, ignore_index=True,
           keys=None, levels=None, names=None, verify_integrity=False,
           copy=True)
 
-# Step 1: treatments and transformations. 
-# Find the number of people they are travelling with. This includes non sibling/spouse copmanions. 
-# Find the maximum number between parch + sibsp + 1 and the number of occurrences of the ticket number. 
+# Step 1: Party Size Treatment 
+
+# Find the number of people they are travelling with. This includes non sibling/spouse companions. 
+# Find the maximum number between parch + sibsp + 1 and the number of times their ticket number appears in the concatenated list between training and testing set. 
+# (Everyone in the same travelling party has the same ticket number) 
 
 for i, row in together.iterrows():
     parch = together.ix[i, 'Parch']
@@ -25,7 +28,23 @@ for i, row in together.iterrows():
     temp = together['Ticket'].value_counts()
     together.ix[i, 'PartySize'] = max(temp[ticket], parch + sibsp + 1)
 
-# Find the different types of titles. Using titles to seperate spouse and sibling in SibSp. 
+# Bucket party size 
+
+for i, row in together.iterrows():
+    if together.ix[i, 'PartySize'] == 0: 
+        together.ix[i, 'PartySize1'] = '0'
+    elif together.ix[i, 'PartySize'] == 1: 
+        together.ix[i, 'PartySize1'] = '1'
+    elif together.ix[i, 'PartySize'] in (2,3,4): 
+        together.ix[i, 'PartySize1'] = '2 to 4'
+    else: 
+        together.ix[i, 'PartySize1'] = '5+'
+
+
+# Step 2: Seperating Sibling and Spouse 
+
+# Find the different types of name titles. Use titles to determine if they have a sibling/spouse using the SibSp variable. 
+# SibSp is the sum of sibling(s) and spouse they are travelling with. 
 
 titles = [] 
 
@@ -33,10 +52,12 @@ for i, row in together.iterrows():
     if re.findall(', [a-zA-Z]+.', together.ix[i, 'Name']) not in titles: 
         titles.append(re.findall(', [a-zA-Z]+.', together.ix[i, 'Name']))
 
-# These were the titles: Mr, Mrs, Miss, Master, Don, Rev, Dr, Mme, Ms, Major, Lady, Sir, Mlle, Col, Capt, the, Jonkheer, Dona 
-# Has Spouse determination (male): if travelling with a female companion (validated through ticket number) who is married with their last name -> married; otherwise -> unmarried 
-# Has Spouse determination (female): Has title "mrs/mme/lady" and SibSp > 0 -> has spouse
-# Has Sib determination: what is left from SibSp after doing spouse reduction 
+# These are the titles: Mr, Mrs, Miss, Master, Don, Rev, Dr, Mme, Ms, Major, Lady, Sir, Mlle, Col, Capt, the, Jonkheer, Dona 
+# Has Spouse determination (male): if travelling with a married female companion (validated through ticket number) who share their last name and SibSp > 0 -> married; otherwise -> unmarried 
+# Has Spouse determination (female): Has title "mrs/mme/lady" and SibSp > 0 -> has spouse; otherwise -> unmarried 
+# Has Sib determination: subtract one from SibSp if they are married. If remaining SibSp count > 0, then they have a sibling. 
+
+# Note: as HasSib and HasSpouse (HasSp) tells a similar story as SibSp and Parch, I chose to exclude SibSp and Parch from the final model in favor of HasSib and HasSpouse (HasSp)
 
 for i, row in together.iterrows(): 
     if ('Mrs.' in together.ix[i, 'Name'].split() or 'Mme' in together.ix[i, 'Name'].split() or 'Lady' in together.ix[i, 'Name'].split()) and together.ix[i, 'SibSp'] > 0 : 
@@ -71,7 +92,9 @@ for i, row in together.iterrows():
         together.ix[i, 'HasSp'] = 0
         together.ix[i, 'HasSib'] = 0 
 
-# Creating Age Buckets. Visualize changes in survival_rates through age. 
+# Step 3: Creating Age Buckets 
+
+# Visualize changes in survival rate against age 
 
 age = train['Age'].drop_duplicates()
 survival_by_age = {}
@@ -80,8 +103,6 @@ for i in age:
     survival_rates = train.ix[train['Age'] == i]
     survival_by_age[i] = float(sum(survival_rates['Survived']))/float((max(len(survival_rates), 1)))
 
-import matplotlib.pylab as plt
-
 lists = sorted(survival_by_age.items())
 
 x,y = zip(*lists)
@@ -89,7 +110,8 @@ x,y = zip(*lists)
 plt.scatter(x,y)
 plt.show()
 
-# Based on graph, split ages based on: 15 or younger, 15 - 60 and over 60  
+# Based on graph, split ages accordingly: 15 or younger, 15 - 60 and over 60; add in category for unknown ages 
+
 for i, row in together.iterrows():
     if together.ix[i, 'Age'] > 0 and together.ix[i, 'Age'] <= 15: 
         together.ix[i, 'Age1'] = '15 and under'
@@ -102,28 +124,27 @@ for i, row in together.iterrows():
             together.ix[i, 'Age1'] = '15 to 60'
         else: 
             together.ix[i, 'Age1'] = '15 and under'
-    
-# Determine if they are a crew member (crew members pay no fare) 
+ 
+ # Step 4: Determine if they are a crew member   
+          
+# Crew members pay no fare 
+# Death rates of crew member significantly higher than regular passengers
+
 for i, row in together.iterrows():
     if together.ix[i, 'Fare'] == 0: 
         together.ix[i, 'IsCrew']= 1
     else: 
         together.ix[i, 'IsCrew'] = 0 
 
-# Bucket party size 
-for i, row in together.iterrows():
-    if together.ix[i, 'PartySize'] == 0: 
-        together.ix[i, 'PartySize1'] = '0'
-    elif together.ix[i, 'PartySize'] == 1: 
-        together.ix[i, 'PartySize1'] = '1'
-    elif together.ix[i, 'PartySize'] in (2,3,4): 
-        together.ix[i, 'PartySize1'] = 'a2-4'
-    else: 
-        together.ix[i, 'PartySize1'] = '5+'
+# Step 5: Check for feature correlations 
 
-# Check for correlations 
+# Possible interaction effects to be considered in model selection: 
+# PartySize x HasSib, PClass x Fare
 
-plt.matshow(together.corr())
+corr = together.corr() 
+sns.heatmap(corr, 
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values)
 
 # Split treated data back into train and test for model training and testing  
 
